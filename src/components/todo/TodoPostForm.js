e3de2d39fa663450842dfd3afe7ea75d454194e8
ashikbhuyan17 <ashikbhuyan.swe.diu@gmail.com>
2022-08-18 13:04:10 +0600

    todo form design

--- src/components/todo/TodoPostForm.js
+++ src/components/todo/TodoPostForm.js
@@ -4,22 +4,22 @@ const TodoPostForm = () => {
     return (
         <main className='flex justify-center items-start h-screen bg-gray-100'>
 
-            <div className='bg-white w-3/6 p-6 rounded shadow-sm mt-16'>
+            <div className='bg-white w-6/12 lg:w-4/12 p-6 rounded shadow-sm mt-36'>
                 <form action="">
                     <div className="title mb-4">
                         <label htmlFor="title" className='text-gray-500 font-semibold'>Title</label>
-                        <input type="text" className='w-full lg:w-2/4 lg:block bg-gray-50 py-2 text-gray-500 px-1 outline-none' />
+                        <input type="text" className='w-full  bg-gray-50 py-2 text-gray-500 px-1 outline-none' />
                     </div>
                     <div className="description mb-4">
                         <label htmlFor="description" className='text-gray-500 font-semibold'>Description</label>
-                        <input type="text" className='w-full lg:w-2/4 lg:block bg-gray-50 py-2 text-gray-500 px-1 outline-none' />
+                        <input type="text" className='w-full  bg-gray-50 py-2 text-gray-500 px-1 outline-none' />
                     </div>
                     <div className="status mb-4">
                         <label htmlFor="description" className='text-gray-500 font-semibold'>Status</label>
                         {/* <input type="text" className='w-full lg:w-2/4 lg:block bg-gray-50 py-2 text-gray-500 px-1 outline-none' /> */}
-                        <select name="" id="" className='w-full lg:w-2/4 lg:block bg-gray-50 p-2 text-gray-500 outline-none'>
-                            <option value="0" className='text-gray-500 outline-none hover:bg-slate-300'>Select status:</option>
-                            <option value="active" className='text-gray-500 outline-none hover:bg-slate-300'>Active</option>
+                        <select name="" id="" className='w-full  bg-gray-50 p-2 text-gray-500 outline-none'>
+                            <option value="0" >Select status:</option>
+                            <option value="active">Active</option>
                             <option value="inactive">Inactive</option>
                         </select>
                     </div>
